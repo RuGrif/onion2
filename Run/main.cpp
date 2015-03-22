f1bbdcc19a6033b0995f08f26066ab823fd8cce8
Anton Terekhov <aterekhov@aligntech.com>
2015-03-22 11:35:09 +0300

    Update load test

--- Run/main.cpp
+++ Run/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 #include "..\Collision\Collider.h"
+#include "..\Collision\AABBCollider.h"
 #include "..\IO\STL.h"
 #include "..\IO\Mesh.h"
 #include "..\QEdge\Utils.h"
 #include <chrono>
 
 
-void main() try
+void collision0() try
 {
   auto t0 = std::chrono::high_resolution_clock::now();
 
-  QEdge_NS::Shape a = IO_NS::readSTL( L"box.stl" );
+  QEdge_NS::Shape a = IO_NS::readSTL( L"distort torus.stl" );
   QEdge_NS::Shape b = IO_NS::readSTL( L"triple torus.stl" );
 
   auto fa = QEdge_NS::allFaces( a );
@@ -33,4 +34,45 @@ catch( const std::exception& e )
 catch( ... )
 {
   std::cout << "(unknown error)" << std::endl;
+}
+
+
+void collision1() try
+{
+  auto t0 = std::chrono::high_resolution_clock::now();
+
+  QEdge_NS::Shape a = IO_NS::readSTL( L"distort torus.stl" );
+  QEdge_NS::Shape b = IO_NS::readSTL( L"triple torus.stl" );
+
+  auto fa = QEdge_NS::allFaces( a );
+  auto fb = QEdge_NS::allFaces( b );
+
+  Math_NS::Grid g;
+
+  Collision_NS::AABBTree ta( fa, g );
+  Collision_NS::AABBTree tb( fb, g );
+
+  Collision_NS::AABBCollider collider;
+
+  collider.collideAll( ta, tb );
+
+  IO_NS::writeMesh( collider.collider().graph(), L"box - triple torus intersection (AABB).mesh" );
+
+  auto t1 = std::chrono::high_resolution_clock::now();
+
+  std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>( t1 - t0 ).count() << " ms" << std::endl;
+}
+catch( const std::exception& e )
+{
+  std::cout << e.what() << std::endl;
+}
+catch( ... )
+{
+  std::cout << "(unknown error)" << std::endl;
+}
+
+
+void main()
+{
+  collision1();
 }
