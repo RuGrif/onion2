95fe5073e9a21b8740baeaec27c749d1c69251f1
Anton Terekhov <uagrif@gmail.com>
2016-03-27 15:03:18 -0400

    switch back on int128

--- UnitTest/Tools.h
+++ UnitTest/Tools.h
@@ -12,10 +12,13 @@ template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString
 template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const QEdge_NS::Vert& v ) { return v ? v->tag() : L"null vert"; }
 template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const QEdge_NS::Face& f ) { return f ? f->tag() : L"null face"; }
 
-template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const Math_NS::Vector3L::Type& t ) { std::wstringstream b; b << t; return b.str(); }
-
-template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const Math_NS::Vector3I& v ) { return std::to_wstring( v.x ) + L", " + std::to_wstring( v.y ) + L", " + std::to_wstring( v.z ); }
-template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const Math_NS::Vector3L& v ) { std::wstringstream b; b << v.x << L", " << v.y << L", " << v.z; return b.str(); }
-template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const Math_NS::Vector3D& v ) { return std::to_wstring( v.x ) + L", " + std::to_wstring( v.y ) + L", " + std::to_wstring( v.z ); }
 
 void label( QEdge_NS::Edge, const std::wstring& = L"e" );
+
+
+namespace Microsoft { namespace VisualStudio { namespace CppUnitTestFramework {
+
+template <typename T> std::wstring ToString( const boost::multiprecision::number<T>& i ) { return ToString( i.str() ); }
+template <typename T> std::wstring ToString( const Math_NS::Vector3<T>& v ) { return ToString( v.x ) + L", " + ToString( v.y ) + L", " + ToString( v.z ); }
+
+}}}
