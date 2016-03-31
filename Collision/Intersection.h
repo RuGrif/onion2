cee6e4bb7a1de9f4efb29de141606792bce280b9
Anton Terekhov <uagrif@gmail.com>
2016-03-30 23:14:16 -0400

    utility to compute point coordinates from xpoint

--- Collision/Intersection.h
+++ Collision/Intersection.h
@@ -88,4 +88,7 @@ namespace Collision_NS
   
   template <typename A, typename B>
   XPointID makeXPointID( const A& a, const B& b ) { return makeXPointID( id( a ), id( b ) ); }
+
+
+  template <typename A, typename B> Math_NS::Vector3D point( const XPoint<A, B>& p ) { return point( p.first, p.second ); }
 }
