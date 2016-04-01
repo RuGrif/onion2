c37c6859c477071b97156bd49b6263fc0f0a7988
Anton Terekhov <aterekhov@aligntech.com>
2016-04-01 00:13:46 -0400

    get rid of copy paste in shape utils

--- QEdge/Utils.cpp
+++ QEdge/Utils.cpp
@@ -3,35 +3,35 @@
 #include <numeric>
 
 
+namespace
+{
+  template <typename ID>
+  std::vector<QEdge_NS::Edge> all( const QEdge_NS::Shape& i_shape, ID id )
+  {
+    std::vector<QEdge_NS::Edge> out;
+    std::unordered_set<size_t> visited;
+
+    for( QEdge_NS::Edge e : i_shape )
+    {
+      if( visited.insert( id( e ) ).second ) out.push_back( e );
+      e = e.sym();
+      if( visited.insert( id( e ) ).second ) out.push_back( e );
+    }
+
+    return out;
+  }
+}
+
+
 std::vector<QEdge_NS::Edge> QEdge_NS::allVerts( const Shape& i_shape )
 {
-  std::vector<Edge> out;
-  std::unordered_set<Vert*> visited;
-
-  for( Edge e : i_shape )
-  {
-    if( visited.insert( &e.o() ).second ) out.push_back( e );
-    e = e.sym();
-    if( visited.insert( &e.o() ).second ) out.push_back( e );
-  }
-
-  return out;
+  return ::all( i_shape, []( Edge e ) { return e.o().id(); } );
 }
 
 
 std::vector<QEdge_NS::Edge> QEdge_NS::allFaces( const Shape& i_shape )
 {
-  std::vector<Edge> out;
-  std::unordered_set<Face*> visited;
-
-  for( Edge e : i_shape )
-  {
-    if( visited.insert( &e.l() ).second ) out.push_back( e );
-    e = e.sym();
-    if( visited.insert( &e.l() ).second ) out.push_back( e );
-  }
-
-  return out;
+  return ::all( i_shape, []( Edge e ) { return e.l().id(); } );
 }
 
 
